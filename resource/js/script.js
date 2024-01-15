678635566e4a6a87772c980675ba314e31dc2677
Doug Sá <79709843+darneees@users.noreply.github.com>
2024-01-15 09:21:12 -0300

    Add files via upload

---
+++ resource/js/script.js
@@ -0,0 +1,31 @@
+// alert email
+
+document.getElementById('newsletterForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    var emailValue = document.getElementById('email').ariaValueMax;
+
+    setTimeout(function () {
+        alert('Um email de confirmação será enviado em breve para ' + emailValue);
+        document.getElementById('email').value = '';
+    }, 4000);
+});
+
+
+// top button
+
+let myButton = document.getElementById('scrollTop');
+window.onscroll = function() { scrollFunction()};
+
+function scrollFunction() {
+    if( document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myButton.style.display = 'flex';
+    } else {
+        myButton.style.display = 'none';
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
